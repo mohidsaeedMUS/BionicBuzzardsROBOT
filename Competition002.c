35c1efbef0905eaa1aae558d5efe30650ca3e6ec
mohidsaeedMUS <45521362+mohidsaeedMUS@users.noreply.github.com>
2018-12-06 16:21:51 -0600

    Update Competition002.c
    Autonomous Quick Debug Button + Cancel Button
    
    NOTE TO SELF: IMPLEMENT TO AUTONOMOUS AND THEN REMOVE BEFORE COMPETITION!
    
    Co-Authored-By: MUSMohidSaeed <mohidsaeedmus@users.noreply.github.com>

--- Competition002.c
+++ Competition002.c
@@ -3,21 +3,6 @@
 #pragma config(Motor,  port3,           TopLeft,       tmotorVex393_MC29, openLoop)
 #pragma config(Motor,  port4,           TopRight,      tmotorVex393_MC29, openLoop, reversed)
 
-/*Programmed by:
-
-//////////////////////////////////////
-___  ___      _     _     _ _____
-|  \/  |     | |   (_)   | /  ___|
-| .  . | ___ | |__  _  __| \ `--.
-| |\/| |/ _ \| '_ \| |/ _` |`--. \
-| |  | | (_) | | | | | (_| /\__/ /
-\_|  |_/\___/|_| |_|_|\__,_\____/
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-*/
-
-
-
 // This code is for the VEX cortex platform
 #pragma platform(VEX2)
 
@@ -64,8 +49,43 @@ task usercontrol()
 		motor[TopRight]= vexRT[Ch2];
 		motor[BottomLeft] = vexRT[Ch3];
 		motor[BottomRight] = 	vexRT[Ch2];
-	}
 
+
+//Button-press autonomous testing, for QUICK DEBUGGING purposes; remove for competition
+
+//AUTONOMOUS QUICK DEBUG
+
+		if(vexRT[Btn8U] == 1)
+		{
+			//AUTONOMOUS GO HERE
+		motor[TopLeft] = 127;
+		motor[TopRight] = 127;
+		motor[BottomLeft] = 127;
+		motor[BottomRight] = 127;
+		}
+
+//AUTONOMOUS CANCEL
+
+		else if(vexRT[Btn8R] == 1)
+		{
+			//SETS ALL MOTORS TO 0
+			motor[TopLeft] = 0;
+			motor[TopRight] = 0;
+			motor[BottomLeft] = 0;
+			motor[BottomRight] = 0;
+		}
+
+//IF BUTTON NOT PRESSED AT ALL
+		else
+		{
+		 //MOTORS DONT DO ANYTHING
+			motor[TopLeft] = 0;
+			motor[TopRight] = 0;
+			motor[BottomLeft] = 0;
+			motor[BottomRight] = 0;
+			}
+
+	}
 
 
 }
